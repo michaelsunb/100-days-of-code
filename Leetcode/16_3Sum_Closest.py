a97ba8ed6d7f8b0ba625feeb31eb5e020a8610e7
michaelsunb <michael.baluyos@gmail.com>
2022-10-09 19:18:31 +1100

    #Day11 Complete Leetcode problem number 16

---
+++ Leetcode/16_3Sum_Closest.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums_len = len(nums)
+        nums.sort()
+        closest = float("inf")
+        for i in range(nums_len - 2):
+            left, right = i + 1, nums_len - 1
+            while left < right:
+                cur_sum = nums[i] + nums[left] + nums[right]
+                if abs(target - cur_sum) < abs(target - closest):
+                    closest = cur_sum
+                if cur_sum == target:
+                    return cur_sum
+                elif cur_sum < target:
+                    left += 1
+                else:
+                    right -= 1
+
+        return closest
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.threeSumClosest([-1,2,1,-4], 1))
+    print(s.threeSumClosest([0,0,0], 1))
